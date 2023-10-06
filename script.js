9140778ab280c308719d8511a53e9eeea6d72f68
Thayna Isalto <133373567+thaynaisalto@users.noreply.github.com>
2023-10-05 22:21:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+const kitty = document.querySelector('.kitty');
+const pipe = document.querySelector('.pipe');
+const cloud = document.querySelector('.cloud');
+
+const gameOver = document.querySelector('.game-over');
+const restartButton = document.querySelector('.restart');
+
+const jump = () => {
+
+    kitty.classList.add('jump');
+
+    setTimeout(() => {
+
+        kitty.classList.remove('jump');
+
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const kittyPosition = +window.getComputedStyle(kitty).bottom.replace('px', '');
+    const cloudPosition = +window.getComputedStyle(cloud).left.replace('px', '');
+
+    if (pipePosition <= 100 && pipePosition > 0 && kittyPosition < 60) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        kitty.style.animation = 'none';
+        kitty.style.bottom = `${kittyPosition}px`;
+
+        kitty.src = './imagens/game-over.png';
+        kitty.style.width = '70px';
+        kitty.style.marginLeft = '35px';
+
+        cloud.style.animation = 'cloud 20s infinite linear';
+        cloud.style.left = `${cloudPosition}px`;
+
+        gameOver.style.visibility = 'visible';
+
+        clearInterval(loop);
+    }
+}, 10);
+
+const restart = () => {
+
+    gameOver.style.visibility = 'hidden';
+
+    pipe.style.animation = 'pipe-animations 1.5s infinite linear';
+    pipe.style.left = ``;
+
+    kitty.src = './imagens/kitty.webp';
+    kitty.style.width = '130px';
+    kitty.style.bottom = '0px';
+    kitty.style.marginLeft = '';
+    kitty.style.animation = '';
+
+    cloud.style.left = ``;
+
+    const loop = setInterval(() => {
+
+        const pipePosition = pipe.offsetLeft;
+        const kittyPosition = +window.getComputedStyle(kitty).bottom.replace('px', '');
+        const cloudPosition = +window.getComputedStyle(cloud).left.replace('px', '');
+    
+        if (pipePosition <= 100 && pipePosition > 0 && kittyPosition < 60) {
+    
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipePosition}px`;
+    
+            kitty.style.animation = 'none';
+            kitty.style.bottom = `${kittyPosition}px`;
+    
+            kitty.src = './imagens/game-over.png';
+            kitty.style.width = '70px';
+            kitty.style.marginLeft = '35px';
+    
+            cloud.style.animation = 'cloud 20s infinite linear';
+            cloud.style.left = `${cloudPosition}px`;
+    
+            gameOver.style.visibility = 'visible';
+    
+            clearInterval(loop);
+        }
+    }, 10);
+}
+
+document.addEventListener('keydown', jump);
+document.addEventListener('touchstart', jump);
+
+restartButton.addEventListener('click', restart);
